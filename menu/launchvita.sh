196b24438e7967c803c48a440c52ed87bb632330
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2019-10-29 08:29:29 -0700

    Update launchvita.sh

--- menu/launchvita.sh
+++ menu/launchvita.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Adjust --speed=1.21 in increments of .10 if you want to reduce latency, you may als otry other "modifiers" to scale, smoth, etc, but can impact preformance
+#Adjust --speed=1.21 in increments of .05 to.10 to reduce latency, you may also try other "modifiers" to upscale, smooth, etc but can impact preformance
 mpv --fullscreen --profile=low-latency --fps=60 --framedrop=no --speed=1.21 --really-quiet /dev/video0
 
 #Launch stream with latency. 
